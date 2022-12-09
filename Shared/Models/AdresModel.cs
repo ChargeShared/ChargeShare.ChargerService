0618a99fe7e0dacf51b810580342a4b499834467
JustFalco <falco@wolkorte.nl>
2022-12-09 21:42:48 +0100

    Added AdresModel in shared folder

--- Shared/Models/AdresModel.cs
+++ Shared/Models/AdresModel.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Shared.Models
+namespace Shared.Models;
+
+
+public class AdresModel
 {
-    internal class AdresModel
-    {
-    }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
+    public string Region { get; set; }
+    public string Province { get; set; }
+    public string HouseNumber { get; set; }
+    public string HouseAddition { get; set; }
 }
+
